022ea2511bcf5817890565eb5e43a03b6d740f09
hajune <blackjune@kakao.com>
2022-12-18 14:34:12 +0900

    react useState & useEffect learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (e) => setKeyword(e.target.value);
+    // console.log('안녕?')
+
+    // * 한번만 실행
+    useEffect(() => {
+        console.log('한번만 실행');
+    }, []);
+
+    // * keyword가 지켜보는 경우 실행
+    useEffect(() => {
+        if(keyword !== "" && keyword.length > 5) {
+            console.log('키워드가 변화할 때 실행 ', keyword)
+        }
+    }, [keyword])
+
+    // * 특정경우(keyword, counter) 지켜보는 경우 실행
+    /*useEffect(() => {
+        console.log('searchbar, counter');
+    },[keyword, counter]);*/
+
+    return (
+        <div>
+            {<input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder={"get ready search"}
+            />}
+                {/*<h1 className={styles.title}>Welcom back!!!</h1>*/}
+                <h3 className={styles.title}>{counter}</h3>
+                {/*<Button text={"Continue"}/>*/}
+                {/*<Button onClick={onClick} text={"click Me!"}/>*/}
+                <button onClick={onClick}>Click me!</button>
+        </div>
+);
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import {PropTypes} from "prop-types";
+import styled from "./Button.module.css";
+
+function Button({text}) {
+
+    return (
+        <div>
+            <button className={styled.btn}>{text}</button>
+        </div>
+    );
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+}
+
+export default Button;
