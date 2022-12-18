b57721a526c200c25d52801548664bdc6618e09d
hajune <blackjune@kakao.com>
2022-12-18 15:45:44 +0900

    useEffect CleanUP Ex learn

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
 import styles from "./App.module.css";
 import {useState, useEffect} from "react";
+import {logDOM} from "@testing-library/react";
+
+function Hello() {
+    function byFn() {
+        console.log('byFn')
+    }
+    // * destroyed 상태를 확인하려면 return 을 하면 된다.
+    /*function hiFn() {
+        console.log('hiFn')
+        return byFn;
+    }*/
+    useEffect(() => {
+        console.log('hiFn');
+        return () => console.log('byFn');
+    },[])
+    return <div>Hello!</div>
+}
 
 function App() {
-
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (e) => setKeyword(e.target.value);
-    // console.log('안녕?')
-
-    // * 한번만 실행
-    useEffect(() => {
-        console.log('한번만 실행');
-    }, []);
-
-    // * keyword가 지켜보는 경우 실행
-    useEffect(() => {
-        if(keyword !== "" && keyword.length > 5) {
-            console.log('키워드가 변화할 때 실행 ', keyword)
-        }
-    }, [keyword])
-
-    // * 특정경우(keyword, counter) 지켜보는 경우 실행
-    /*useEffect(() => {
-        console.log('searchbar, counter');
-    },[keyword, counter]);*/
-
+     const [showing, setShowing] = useState(false);
+     const onClick = () => setShowing((prev) => !prev)
     return (
         <div>
-            {<input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder={"get ready search"}
-            />}
-                {/*<h1 className={styles.title}>Welcom back!!!</h1>*/}
-                <h3 className={styles.title}>{counter}</h3>
-                {/*<Button text={"Continue"}/>*/}
-                {/*<Button onClick={onClick} text={"click Me!"}/>*/}
-                <button onClick={onClick}>Click me!</button>
+            {showing ? <Hello/> : null}
+            <button onClick={onClick}>{showing ? "hide" : "show"}</button>
         </div>
-);
+    );
 }
 
 export default App;
