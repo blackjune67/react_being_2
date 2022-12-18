e1c477f17e93f4c1701b52446e896363c96d8e84
hajune <blackjune@kakao.com>
2022-12-18 16:01:13 +0900

    start react todo list app

--- src/App.js
+++ src/App.js
@@ -2,31 +2,19 @@ import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 import {logDOM} from "@testing-library/react";
 
-function Hello() {
-    function byFn() {
-        console.log('byFn')
-    }
-    // * destroyed 상태를 확인하려면 return 을 하면 된다.
-    /*function hiFn() {
-        console.log('hiFn')
-        return byFn;
-    }*/
-    useEffect(() => {
-        console.log('hiFn');
-        return () => console.log('byFn');
-    },[])
-    return <div>Hello!</div>
-}
-
 function App() {
-     const [showing, setShowing] = useState(false);
-     const onClick = () => setShowing((prev) => !prev)
-    return (
-        <div>
-            {showing ? <Hello/> : null}
-            <button onClick={onClick}>{showing ? "hide" : "show"}</button>
-        </div>
-    );
+    const [todo, setTodo] = useState("");
+    const onChange = (e) => {
+        console.log('==> : ', todo);
+        setTodo(e.target.value);
+    };
+
+    return <div>
+        <input
+            onChange={onChange}
+            value={todo}
+            type="text" placeholder={"Write your to do..."}/>
+    </div>
 }
 
 export default App;
