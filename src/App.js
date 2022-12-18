fe8f86664565cecb88d0d65766131f3dc04d7c6a
hajune <blackjune@kakao.com>
2022-12-18 23:15:10 +0900

    todo list create

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import {useState, useEffect} from "react";
 import {logDOM} from "@testing-library/react";
 
 function App() {
-    const [todo, setTodo] = useState("");
-    const onChange = (e) => {
-        console.log('==> : ', todo);
-        setTodo(e.target.value);
-    };
+    const [toDo, setTodo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e) => setTodo(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (toDo === "") {
+            return;
+        }
 
-    return <div>
-        <input
-            onChange={onChange}
-            value={todo}
-            type="text" placeholder={"Write your to do..."}/>
-    </div>
+        setToDos(currentArray => [toDo, ...currentArray]);
+        setTodo("");
+    };
+    console.log(toDos);
+    return (
+        <div>
+            <h1>To Do List ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text" placeholder={"Write your to do..."}
+                />
+                <button>Add To Do</button>
+                <hr/>
+                <ul>
+                    {toDos.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </form>
+        </div>
+    );
 }
 
 export default App;
